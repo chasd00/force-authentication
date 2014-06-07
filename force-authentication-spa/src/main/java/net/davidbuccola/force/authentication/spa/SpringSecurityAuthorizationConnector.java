6f28f28067f9f31cf6b0fdb9c01a224c6a78098b
David Buccola <david.buccola@gmail.com>
2014-06-07 08:27:41 -0700

    Incremental fixes to work with newer version of SPA.

--- force-authentication-spa/src/main/java/net/davidbuccola/force/authentication/spa/SpringSecurityAuthorizationConnector.java
+++ force-authentication-spa/src/main/java/net/davidbuccola/force/authentication/spa/SpringSecurityAuthorizationConnector.java
@@ -5,13 +5,15 @@
  */
 package net.davidbuccola.force.authentication.spa;
 
-import com.force.spa.AuthorizationConnector;
-import net.davidbuccola.force.authentication.AuthenticationUtils;
-import net.davidbuccola.force.authentication.ForceAuthenticationToken;
+import java.net.URI;
+
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
-import java.net.URI;
+import com.force.spa.AuthorizationConnector;
+
+import net.davidbuccola.force.authentication.AuthenticationUtils;
+import net.davidbuccola.force.authentication.ForceAuthenticationToken;
 
 /**
  * An {@link AuthorizationConnector} implementation that knows how to obtain authorization and instance information from
@@ -24,12 +26,18 @@ public class SpringSecurityAuthorizationConnector implements AuthorizationConnec
     @Override
     public String getAuthorization() {
         ForceAuthenticationToken authentication = AuthenticationUtils.getAuthenticationToken();
-        return "OAuth " + authentication.getAccessToken();
+        return "Bearer " + authentication.getAccessToken();
     }
 
     @Override
     public URI getInstanceUrl() {
         ForceAuthenticationToken authentication = AuthenticationUtils.getAuthenticationToken();
         return URI.create(authentication.getInstanceUrl());
+    }
+
+    @Override
+    public String getUserId() {
+        ForceAuthenticationToken authentication = AuthenticationUtils.getAuthenticationToken();
+        return authentication.getUserId();
     }
 }
